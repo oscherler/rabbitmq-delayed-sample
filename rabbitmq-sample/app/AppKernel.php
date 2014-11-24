7011a623a22dc1d0b97ff5b362cf98189d7f1877
Olivier <olivier@gasser-media.ch>
2014-11-24 12:06:26 +0100

    Installed RabbitMQBundle.

--- rabbitmq-sample/app/AppKernel.php
+++ rabbitmq-sample/app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
+            new OldSound\RabbitMqBundle\OldSoundRabbitMqBundle(),
+            
             new RabbitMQ\SampleBundle\RabbitMQSampleBundle(),
         );
 
