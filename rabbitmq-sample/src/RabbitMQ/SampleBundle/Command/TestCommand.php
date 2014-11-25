5865d1be64c9026bef7f64622be2720541f23c1b
Olivier <oscherler@femto-byte.com>
2014-11-25 14:39:43 +0100

    Created a test command.

---
+++ rabbitmq-sample/src/RabbitMQ/SampleBundle/Command/TestCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RabbitMQ\SampleBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Publish a message to RabbitMQ.
+ */
+class TestCommand extends ContainerAwareCommand
+{
+	/**
+	 * @see Command
+	 */
+	protected function configure()
+	{
+		$this
+			->setName('sample:test')
+			->setDescription('Publishes a delayed message')
+		;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function execute( InputInterface $input, OutputInterface $output )
+	{
+		$producer = $this->getContainer()->get('delayed_producer');
+		
+		$producer->publish( 'Hello', 'working' );
+	}
+}
